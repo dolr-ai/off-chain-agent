5c6d7d4a6f62cf0cbbafd87f1f6d2b9fa5dc11f4
tushar-saxena-yral <tushar@gobazzinga.io>
2025-07-16 23:26:46 +0530

    Revert "chore: print admin principal"
    This reverts commit a4a5deb13560b21dd7ed6c7e34a10aaa7135e12d.

--- src/app_state.rs
+++ src/app_state.rs
@@ -149,7 +149,8 @@ pub fn init_yral_metadata_client(conf: &AppConfig) -> MetadataClient<true> {
 pub async fn init_agent() -> Agent {
     #[cfg(not(any(feature = "local-bin", feature = "use-local-agent")))]
     {
-        let pk = env::var("CANISTERS_GLOBAL_ADMIN_PEM").expect("$CANISTERS_GLOBAL_ADMIN_PEM is not set");
+        let pk =
+            env::var("CANISTERS_GLOBAL_ADMIN_PEM").expect("$CANISTERS_GLOBAL_ADMIN_PEM is not set");
 
         let identity = match ic_agent::identity::BasicIdentity::from_pem(
             stringreader::StringReader::new(pk.as_str()),
@@ -165,13 +166,7 @@ pub async fn init_agent() -> Agent {
             .with_identity(identity)
             .build()
         {
-            Ok(agent) => {
-                println!(
-                    "CANISTERS_GLOBAL_ADMIN_PEM principal: {:?}",
-                    agent.get_principal().map(|p| p.to_text())
-                );
-                agent
-            }
+            Ok(agent) => agent,
             Err(err) => {
                 panic!("Unable to create agent, error: {:?}", err);
             }
