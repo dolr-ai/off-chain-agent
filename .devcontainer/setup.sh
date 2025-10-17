5f44419048c7acdb626029847af2af8003e22a53
Saikat Das <saikatdas0790@gmail.com>
2025-10-17 05:22:08 +0000

    add initial devcontainer setup with Dockerfile, devcontainer.json, and setup script

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,19 @@
+# Install cargo binstall
+curl -L https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-x86_64-unknown-linux-musl.tgz -o cargo-binstall.tgz;
+tar -xzf cargo-binstall.tgz;
+mkdir -p ~/.local/bin;
+mv ./cargo-binstall ~/.local/bin/cargo-binstall;
+chmod +x ~/.local/bin/cargo-binstall;
+rm cargo-binstall.tgz;
+
+# Fetch git submodules for ml-feed protobuf contracts
+git submodule update --init --recursive;
+
+# # Install cargo-leptos with cargo binstall
+# cargo binstall cargo-leptos --no-confirm;
+
+# # Install leptosfmt using cargo binstall
+# cargo binstall leptosfmt --no-confirm;
+
+# # Enable the env file
+# cp .env.example .env;
